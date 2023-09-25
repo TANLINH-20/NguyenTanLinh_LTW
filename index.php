bed37b80bddbb3cbfb3f83cb6106d57745672154
TanLinh <linhnguyentan24@gmail.com>
2023-09-25 19:57:40 +0700

    add

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-        echo "Nguyen Tan Linh";
-    ?>
-</body>
-</html>
+<?php
+require_once 'vendor/autoload.php';
+
+use Src\Route; //Call class
+
+Route::route_site();
+?>
